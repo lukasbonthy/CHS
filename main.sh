5ea34d43fc3465b977095707ec077df0898d6cb1
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2025-01-18 15:50:30 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,7 +9,7 @@ sed -i 's/${SERVER}/'"$SERVER"'/g' config.yml
 java -Xmx1024M -Xms1024M -jar bungee.jar &
 
 # Wait for Bungee to start (adjust the sleep time as necessary)
-sleep 10
+sleep 60
 
 # Run the confirm-code command for EaglerXBungee after Bungee has started
 java -jar /bungee/plugins/EaglerXBungee-1.2.6.jar confirm-code 1ac4e20429
